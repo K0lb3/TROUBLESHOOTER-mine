04d7419b0b0f1f3f34f90ec70f5438823b4647c2
K0lb3 <rkolbe96@gmail.com>
2021-07-19 11:44:35 +0200

    Update path.py

--- path.py
+++ path.py
@@ -1,13 +1,17 @@
 import os
 
+LOCAL = os.path.dirname(os.path.realpath(__file__))
+ROOT = os.path.dirname(LOCAL)
+
 # path of the game
 GAME = r"E:\Program Files (x86)\Steam\steamapps\common\Troubleshooter"
+# in case the path isn't configured, assume that the scripts are in a sub-dir of the Troubleshooter dir
+if not os.path.exists(GAME):
+    GAME = os.path.dirname(ROOT)
 
 # source dirs
 PACK = os.path.join(GAME, "Package")
 DUMP = os.path.join(GAME, "Release","bin","zip_dump")
 
-# extract dirs
-LOCAL = os.path.dirname(os.path.realpath(__file__))
-ROOT = os.path.dirname(LOCAL)
+# extract dir - where the data will be copied to
 DATA = os.path.join(ROOT, "Original")
