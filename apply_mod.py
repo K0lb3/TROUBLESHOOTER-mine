8a1a5519da40dbe2d278f67e6f10feba6e74fe2d
K0lb3 <rkolbe96@gmail.com>
2021-07-28 12:27:28 +0200

    mod support added

---
+++ apply_mod.py
@@ -0,0 +1,50 @@
+import os
+import xml.etree.ElementTree as ET
+from lib.paths import PACK, MOD, BACKUP
+from lib.crypt import encrypt, decrypt, pack, extract
+from shutil import copy
+import io
+
+
+def main():
+    # os.makedirs(OVERRIDE, exist_ok=True)
+    os.makedirs(BACKUP, exist_ok=True)
+    # 1. load original xml
+    index_fp = os.path.join(PACK, "index.xml")
+    ori_index_fp = os.path.join(PACK, "index")
+    raw_xml = decrypt(ori_index_fp).rstrip(b"\00")
+    # parse index.xml
+    tree = ET.parse(io.BytesIO(raw_xml))
+    root = tree.getroot()
+
+    item_map = {item.get("original"): item for item in root}
+
+    # pack mod files
+    for root, dirs, files in os.walk(MOD):
+        for fp in files:
+            fp = os.path.join(root, fp)
+            ifp = fp[len(MOD) + 1 :]
+            item = item_map.get(ifp, None)
+
+            if item != None:
+                pack_path = os.path.join(PACK, *item.get("pack").split("/"))
+                backup_path = os.path.join(BACKUP, *item.get("pack").split("/"))
+                os.makedirs(os.path.dirname(backup_path), exist_ok=True)
+                # only create a backup if the file size fits, otherwise the backup might be replaced by an old mod
+                if os.path.getsize(pack_path) == item.get(
+                    "csize"
+                ) and not os.path.exists(backup_path):
+                    copy(pack_path, backup_path)
+                pack(fp, pack_path, item)
+                # item.set("pack", f"..\\{os.path.basename(OVERRIDE)}\\{item.get('pack')}")
+                print("Packed", ifp)
+            else:
+                print("Couldn't find", ifp, "in the original index.")
+                print("Import of new assets isn't implemented yet.")
+
+    # save index
+    # copy(ori_index_fp, os.path.join(BACKUP, "index"))
+
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 
 This repo contains scripts that can be used to convert the ``pack`` structure of the game [TROUBLESHOOTER: Abandoned Children](https://store.steampowered.com/app/470310/TROUBLESHOOTER_Abandoned_Children/) into the ``original`` and readable data structure used by the devs.
 
+
 ## Scripts
 
 Python 3.5+ is required to run the scripts.
@@ -14,11 +15,20 @@ or
 
 You have to clone/copy this repo into the Troubleshooter main folder
 e.g. ``Steam\steamapps\common\Troubleshooter\TROUBLESHOOTER-mine``
-or adjust the GAME folder in ``path.py``.
+or adjust the GAME folder in ``lib\paths.py``.
 
 * extract_files.py - decrypts, unpacks and copies the "pack"ed files into the "Data" directory using the original file paths and names
 * extract_imagesets.py - splits the extracted image sets into its single images
+* apply_mod.py - check Modding
 
+
+## Modding
+
+Modding is possible by moving edited files into the ``MOD`` folder set in ``lib\paths.py``.
+The edited files have to have the same path as their original within the ``MOD`` folder, so e.g. if you want to mod ``xml\Option.xml``, then you have to move the modded version to ``MOD\xml\Option.xml``.
+
+The script ``apply_mod.py`` packs all the files from the mod folder into their packed form and replaces the original files with them. It also creates a backup of the original files in the ``BACKUP`` folder set in ``lib\paths.py``.
+So if you want to undo your mod, simply copy all files from ``BACKUP`` into the ``Package`` folder of the game and use ``replace all``.
 
 ## Troublecrypt.exe
 
